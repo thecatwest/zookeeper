dcbf6d9dc0bbca59d66a66e6e19da8c3992be954
TheCatWest <catwestover@outlook.com>
2021-04-26 14:46:02 -0700

    add application

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { animals } = require("./data/animals");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -54,6 +55,6 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-app.listen(3020, () => {
-  console.log(`API server now on port 3020!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
