e4762fd8c8a400ced87f6b6a845a76f6c27476b5
TheCatWest <catwestover@outlook.com>
2021-04-29 22:10:28 -0700

    integrate front end

---
+++ public/assets/js/script.js
@@ -0,0 +1,53 @@
+const $animalForm = document.querySelector('#animal-form');
+
+const handleAnimalFormSubmit = event => {
+  event.preventDefault();
+
+  // get animal data and organize it
+  const name = $animalForm.querySelector('[name="animal-name"]').value;
+  const species = $animalForm.querySelector('[name="species"]').value;
+  const dietRadioHTML = $animalForm.querySelectorAll('[name="diet"]');
+  let diet;
+
+  for (let i = 0; i < dietRadioHTML.length; i += 1) {
+    if (dietRadioHTML[i].checked) {
+      diet = dietRadioHTML[i].value;
+    }
+  }
+
+  if (diet === undefined) {
+    diet = '';
+  }
+
+  const selectedTraits = $animalForm.querySelector('[name="personality"').selectedOptions;
+  const personalityTraits = [];
+  for (let i = 0; i < selectedTraits.length; i += 1) {
+    personalityTraits.push(selectedTraits[i].value);
+  }
+  const animalObject = { name, species, diet, personalityTraits };
+
+  // POST data from front end to back end using fetch() API
+  // first, specify type of request and set method (POST) - allow request to make to proper endpoint on server
+  // then, tell the request what type of data to send and provide that data (JSON) (set type in headers)
+  // similar to using <script> and <link> to incorporate local files, but done through fetch() call
+  fetch('/api/animals', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(animalObject)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert(`Error: ${response.statusText}`);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding an animal!');
+    });
+};
+
+$animalForm.addEventListener('submit', handleAnimalFormSubmit);
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { animals } = require('./data/animals');
 
-const express = require("express");
-const { animals } = require("./data/animals");
-const { type } = require("os");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,6 +10,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+// set up Express.js middleware that instructs the server to make certain files available
+// and not gated behind server endpoint.
+// provide a file path to a location in the app, and instruct server to make these files static resources
+// as a result: all front-end code can now be accessed w/o having specific server endpoint created for it
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -34,30 +39,24 @@ function filterByQuery(query, animalsArray) {
       // so at the end we'll have an array of animals that have every one
       // of the traits when the .forEach() loop is finished
       filteredResults = filteredResults.filter(
-        (animal) => animal.personalityTraits.indexOf(trait) !== -1
+        animal => animal.personalityTraits.indexOf(trait) !== -1
       );
     });
   }
   if (query.diet) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.diet === query.diet
-    );
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
   }
   if (query.species) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.species === query.species
-    );
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
   }
   if (query.name) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.name === query.name
-    );
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
   }
   return filteredResults;
 }
 
 function findById(id, animalsArray) {
-  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
 }
 
@@ -68,7 +67,7 @@ function createNewAnimal(body, animalsArray) {
   // using fs.writeFileSync() (synchronous version of fs.writeFil()) to write to animals.json
   fs.writeFileSync(
     // path.join() method joins the value of __dirname (the directory of the file executing the code in) with the path to animals.json file
-    path.join(__dirname, "./data/animals.json"),
+    path.join(__dirname, './data/animals.json'),
     // save JavaScript array data as JSON; null argument = don't edit existing data
     // 2 = the amount of white space created between variables (makes more legible)
     JSON.stringify({ animals: animalsArray }, null, 2)
@@ -81,22 +80,22 @@ function createNewAnimal(body, animalsArray) {
 // in POST route's callback before creating and adding data, it will now pass through this function above
 // if any data fails validation, the animal will not be created (see app.post for error)
 function validateAnimal(animal) {
-  if (!animal.name || typeof animal.name !== "string") {
+  if (!animal.name || typeof animal.name !== 'string') {
     return false;
   }
-  if (!animal.species || typeof animal.species !== "string") {
+  if (!animal.species || typeof animal.species !== 'string') {
     return false;
   }
-  if (!animal.diet || typeof animal.diet !== "string") {
+  if (!animal.diet || typeof animal.diet !== 'string') {
     return false;
   }
   if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
     return false;
   }
-  return false;
+  return true;
 }
 
-app.get("/api/animals", (req, res) => {
+app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -104,7 +103,7 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-app.get("/api/animals/:id", (req, res) => {
+app.get('/api/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
@@ -113,18 +112,41 @@ app.get("/api/animals/:id", (req, res) => {
   }
 });
 
-app.post("/api/animals", (req, res) => {
+// set a route to the index.html file
+// the '/' route directs us to the root route of the server (used to create a homepage for a server)
+// this route has one job: respond with an HTML page to display in the browser
+// (index.html file never goes to browser, only its contents do)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// set route to animals.html
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+// set route to zookeepers.html
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+// wildcard route - when client makes request for route that does not exist, wildcard route catches these requests
+// wildcard (*) should ALWAYS come last, or it will take precedence over named routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
+  //  if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
-    // res.status().send is response method to relay a message to client making request
-    res.status(400).send("The animal is not properly formatted.");
+    // res.status().send is response method ot relay message to client making request
+    res.status(400).send('The animal is not properly formatted.');
   } else {
-    // add animal to json file and animals array in this function
+    // add animal to json file and animals array in this fn
     const animal = createNewAnimal(req.body, animals);
-    res.json(req.body);
+    res.json(animal);
   }
 });
 
